7066ca161623aef49fe08672747c8607a8b79c32
pedro <progerios@hotmail.com>
2026-01-03 16:27:20 -0300

    feat(core): automate dataset generation in main pipeline

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from data.gerar_dados import gerar
 from model.train import train_and_save
 from model.evaluate import evaluate
 
+print("🔄 Gerando base sintética...")
+gerar()
+
+print("🧠 Treinando modelo...")
 train_and_save()
+
+print("📊 Avaliando modelo...")
 evaluate()
+
+print("✅ Pipeline finalizado com sucesso.")
